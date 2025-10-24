03e315001acfd1755b1197a17bd9e11383e243ba
Hendrik Strydom <hendrik@isdev.co>
2025-10-24 09:24:21 +0200

    feat: add logout endpoint

---
+++ src/server/routes/logout.ts
@@ -0,0 +1,38 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import z from "zod";
+import CONFIG from "../../config";
+import { userModel } from "../../mongo/models/user";
+import { Endpoint } from "../package";
+import { getSecureCookieOptions } from "../util/cookies";
+
+const loginBodySchema = z.object({
+    username: z.string().min(3, "Username must be at least 3 characters").max(20, "Username must not exceed 20 characters"),
+    password: z.string().min(6, "Password must be at least 6 characters").max(50, "Password must not exceed 50 characters"),
+});
+
+export const endpoint = new Endpoint("post", "/v1/logout").withBody(loginBodySchema).onCall(async (req, res) => {
+    const cookies = req.cookies;
+    const token = cookies["token"];
+
+    if (!token) {
+        res.status(401).json({ error: "NO_TOKEN", message: "No token provided" });
+        return;
+    }
+
+    const tokenPayload = jwt.verify(token, CONFIG.jwt.secret);
+
+    if (typeof tokenPayload !== "object" || !("id" in tokenPayload)) {
+        res.status(401).json({ error: "INVALID_TOKEN", message: "Token payload is invalid" });
+        return;
+    }
+
+    const user = await userModel.findOne({ id: tokenPayload.id });
+    if (!user) {
+        res.status(404).json({ error: "NO_USER", message: "User not found" });
+        return;
+    }
+
+    res.cookie("token", "", getSecureCookieOptions({ maxAge: 0 }));
+    res.json({ success: true, message: "Logged out successfully" });
+});
